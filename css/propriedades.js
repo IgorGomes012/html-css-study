b9efb1712421b56e52c05dafd46508c3357b1dcc
igorGomes <gomesoliveirasl02@hotmail.com>
2022-04-18 19:14:10 -0300

    propriedades bastantes usadas css

---
+++ css/propriedades.js
@@ -0,0 +1,21 @@
+// Transbordar
+// É possível, usando overflow, definir o que acontece quando seu conteúdo é muito grande para caber. O uso mais comum é provavelmente adicionar barras de rolagem a um elemento dentro de uma página da Web, por exemplo, um cardelemento de estilo com conteúdo rolável.
+
+// Confira os overflowdocumentos https://developer.mozilla.org/en-US/docs/Web/CSS/overflow
+
+//--------------------------------------------------------------------------------------------------
+
+
+// Opacidade
+// A opacidade é outra fácil que pode ser muito útil em algumas circunstâncias.
+
+// Confira opacityuma definição e alguns exemplos.  https://developer.mozilla.org/en-US/docs/Web/CSS/opacity
+
+//---------------------------------------------------------------------------------------------------------
+
+// Recursos adicionais
+// Ao longo desta lição, vinculamos principalmente os documentos do MDN para cada propriedade... no entanto, existem muitos sites de referência úteis na Web que podem ser úteis e que podem aparecer ao pesquisar esses itens.
+
+// CSS Tricks tem um conteúdo muito bom. Alguns deles parecem menos formais e oficiais do que os documentos do MDN, mas isso significa que eles podem ser mais fáceis de digerir. Às vezes, seus exemplos podem ser mais úteis. Por exemplo, confira suas páginas no fundo abreviado ou estouro .  https://css-tricks.com/almanac/properties
+
+
