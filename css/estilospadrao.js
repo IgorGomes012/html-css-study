7e55aac60cabdc37b37de0342b86f75f0d78d394
igorGomes <gomesoliveirasl02@hotmail.com>
2022-04-19 11:10:23 -0300

    r

--- css/estilospadrao.js
+++ css/estilospadrao.js
@@ -1,21 +1,20 @@
-// Estilos padrão
+Estilos padrão
 
-// Quando você fez o primeiro projeto HTML Recipes , você deve ter notado que alguns elementos já possuem um pouco de estilo aplicado a eles. Um h1, por exemplo, é maior e mais ousado que o texto normal. Os links ( a) são azuis e sublinhados. As listas ( ule ol) têm um monte de preenchimento em torno delas. Os navegadores fazem isso inserindo um pouco de CSS em cada página da web. ( Aqui está a folha de estilo HTML padrão do Chrome )
+Quando você fez o primeiro projeto HTML Recipes , você deve ter notado que alguns elementos já possuem um pouco de estilo aplicado a eles. Um h1, por exemplo, é maior e mais ousado que o texto normal. Os links ( a) são azuis e sublinhados. As listas ( ule ol) têm um monte de preenchimento em torno delas. Os navegadores fazem isso inserindo um pouco de CSS em cada página da web. ( Aqui está a folha de estilo HTML padrão do Chrome )
 
-// O problema com isso é que não há garantia de que navegadores diferentes tenham o mesmo estilo. Em geral, as inconsistências serão bem pequenas, mas elas existem. Além disso, em muitos casos, como desenvolvedor, você acabará desfazendo ou refazendo todo esse estilo padrão para tornar seu site exatamente como você o imagina.
+O problema com isso é que não há garantia de que navegadores diferentes tenham o mesmo estilo. Em geral, as inconsistências serão bem pequenas, mas elas existem. Além disso, em muitos casos, como desenvolvedor, você acabará desfazendo ou refazendo todo esse estilo padrão para tornar seu site exatamente como você o imagina.
 
-// Para combater isso, muitos desenvolvedores iniciam seus projetos com um “CSS Reset”; um arquivo que desfaz os padrões do navegador, para que todos os elementos se comportem da mesma forma em todos os navegadores.
+Para combater isso, muitos desenvolvedores iniciam seus projetos com um “CSS Reset”; um arquivo que desfaz os padrões do navegador, para que todos os elementos se comportem da mesma forma em todos os navegadores.
 
-// Você não precisa usar uma redefinição de CSS. Em muitos casos, você acabará desfazendo ou refazendo muitos dos estilos que uma redefinição fornecerá para você. Você pode decidir se deseja ou não usar um regularmente, mas vale a pena dedicar um tempo para pesquisar alguns agora. Entender exatamente como eles estão fazendo o que estão fazendo é um exercício útil!
-
-//-----------------------
+Você não precisa usar uma redefinição de CSS. Em muitos casos, você acabará desfazendo ou refazendo muitos dos estilos que uma redefinição fornecerá para você. Você pode decidir se deseja ou não usar um regularmente, mas vale a pena dedicar um tempo para pesquisar alguns agora. Entender exatamente como eles estão fazendo o que estão fazendo é um exercício útil!
 
 
 
 
-//O Meyer Reset é quase certamente o mais popular. É muito simples e basicamente remove todos os estilos padrão.
-//https://meyerweb.com/eric/tools/css/reset/
 
-// Normalize.css é outro popular. É um pouco diferente, pois não remove todos os estilos padrão, mas os ajusta um pouco para garantir que os navegadores sejam consistentes. http://nicolasgallagher.com/about-normalize-css/
+O Meyer Reset é quase certamente o mais popular. É muito simples e basicamente remove todos os estilos padrão.
+https://meyerweb.com/eric/tools/css/reset/
 
-// Reboot, Resets and Reasoning é um artigo de truques de CSS que vai um pouco mais a fundo e menciona alguns outros resets populares.  https://css-tricks.com/reboot-resets-reasoning/
+Normalize.css é outro popular. É um pouco diferente, pois não remove todos os estilos padrão, mas os ajusta um pouco para garantir que os navegadores sejam consistentes. http://nicolasgallagher.com/about-normalize-css/
+
+Reboot, Resets and Reasoning é um artigo de truques de CSS que vai um pouco mais a fundo e menciona alguns outros resets populares.  https://css-tricks.com/reboot-resets-reasoning/
